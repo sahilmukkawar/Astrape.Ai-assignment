e34870b94f1822bae326a7de6d309f5ef88ef148
sahilmukkawar <mukkawarsahil99@gmail.com>
2025-09-08 19:25:28 +0530

    changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiBaseUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const apiBaseUrl = import.meta.env.VITE_API_URL || 'https://astrape-ai-assignment-2.onrender.com/api' || 'http://localhost:5000/api';
 
 export const apiClient = axios.create({
   baseURL: apiBaseUrl,
