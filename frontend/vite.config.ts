0b48935ee030136ff0bf92f4e406d89f94cfce81
sahilmukkawar <mukkawarsahil99@gmail.com>
2025-09-08 19:09:44 +0530

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173,
+    port: process.env.PORT ? Number(process.env.PORT) : 5173,
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173,
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
     allowedHosts: [
       'astrape-ai-assignment-3.onrender.com'
     ]
